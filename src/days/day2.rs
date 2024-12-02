a5d9dd4a13ddf27ebce2cba0f8596ff0c4c82f82
MaxL22 <max987651@gmail.com>
2024-12-02 23:22:59 +0100

    d2 p2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -3,45 +3,63 @@ pub fn get_res(path: &str) -> (i32, i32) {
     let mut count = (0, 0);
 
     for line in file.lines() {
-        let mut c: Vec<i32> = line
+        let c: Vec<i32> = line
             .split_whitespace()
             .map(|s| s.parse::<i32>().expect("Err"))
             .collect();
 
         // Part 1
-        let ord = c[0].cmp(&c[1]);
 
+        let ord = c[0].cmp(&c[1]);
         count.0 += 1;
+        let mut c1 = Box::new(true);
+
         for (i, el) in c[0..c.len() - 1].iter().enumerate() {
             let abs = (*el - c[i + 1]).abs();
             if el.cmp(&c[i + 1]) == ord && abs <= 3 && abs > 0 {
                 continue;
             }
             count.0 -= 1;
+            *c1 = false;
             break;
         }
 
         //Part 2
         // Need to remove the "wrong" value
-        count.1 += 1;
-        let mut failsafe = true;
 
-        for mut i in 1..c.len() - 1 {
-            let o1 = c[1].cmp(&c[0]);
-            let abs = (c[i] - c[i - 1]).abs();
+        if *c1 == true {
+            count.1 += 1;
+            continue;
+        }
 
-            if c[i].cmp(&c[i - 1]) == o1 && abs <= 3 && abs > 0 {
-                continue;
+        let mut c2 = Box::new(false);
+
+        // Tries removing all values and finding a combination that works
+        'f: for k in 0..c.len() {
+            *c2 = true;
+            let mut tmpv = c.clone();
+
+            tmpv.remove(k);
+
+            let ord1 = tmpv[0].cmp(&tmpv[1]);
+
+            for (j, el) in tmpv[0..tmpv.len() - 1].iter().enumerate() {
+                let abs = (*el - tmpv[j + 1]).abs();
+
+                if el.cmp(&tmpv[j + 1]) == ord1 && abs <= 3 && abs > 0 {
+                    continue;
+                }
+                *c2 = false;
+                break;
             }
 
-            if failsafe {
-                failsafe = false;
-                c.remove(i);
-                continue;
+            if *c2 == true {
+                break 'f;
             }
+        }
 
-            count.1 -= 1;
-            break;
+        if *c2 == true {
+            count.1 += 1;
         }
     }
     count
