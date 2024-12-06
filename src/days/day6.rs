ef93ea3f5051250e7b22551d58375242df380c11
MaxL22 <max987651@gmail.com>
2024-12-06 18:06:56 +0100

    day 6

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
 enum Direction {
     Up = 0,
     Right,
@@ -5,9 +8,20 @@ enum Direction {
     Left,
 }
 
-fn dir_update(direction: Direction, coord: &mut (i32, i32), forward: bool) {
-    let val;
+impl Direction {
+    fn next(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+}
 
+// Takes a step, forward or backwards, depending on the bool
+fn take_a_step(direction: &Direction, coord: &mut (i32, i32), forward: bool) {
+    let val: i32;
     if forward {
         val = 1
     } else {
@@ -15,10 +29,57 @@ fn dir_update(direction: Direction, coord: &mut (i32, i32), forward: bool) {
     }
 
     match direction {
-        Direction::Up => coord.0 += val,
-        Direction::Down => coord.0 -= val,
+        Direction::Up => coord.0 -= val,
+        Direction::Down => coord.0 += val,
         Direction::Right => coord.1 += val,
         Direction::Left => coord.1 -= val,
+    }
+}
+
+// Paths the thingy, returns true if loopy
+fn pathy(
+    map: &mut Vec<Vec<char>>,
+    mut sc: &mut (i32, i32),
+    count: &mut (i32, i32),
+    direction: &mut Direction,
+    counter: bool,
+) -> bool {
+    // Tracks visited positions
+    let mut hm: HashMap<(i32, i32), Direction> = HashMap::new();
+
+    loop {
+        match map[sc.0 as usize][sc.1 as usize] {
+            // Go forward
+            '.' => {
+                if counter {
+                    count.0 += 1;
+                }
+                map[sc.0 as usize][sc.1 as usize] = 'X';
+            }
+            // There's an obstacle
+            '#' | 'O' => {
+                // Checks if we're in a loop
+                if hm.contains_key(&sc) && *hm.get(sc).unwrap() == *direction {
+                    return true;
+                } else {
+                    hm.insert(*sc, *direction);
+                }
+
+                // Go back 1
+                take_a_step(&direction, &mut sc, false);
+                *direction = direction.next();
+            }
+            // Forward, but already seen
+            _ => (),
+        }
+
+        // Next step
+        take_a_step(&direction, &mut sc, true);
+
+        // Out-of-map termination condition
+        if sc.0 == map.len() as i32 || sc.1 == map[0].len() as i32 || sc.0 < 0 || sc.1 < 0 {
+            return false;
+        }
     }
 }
 
@@ -26,47 +87,53 @@ pub fn get_res(path: &str) -> (i32, i32) {
     let file = std::fs::read_to_string(path).unwrap();
     let mut count = (0, 0);
 
+    // Respectively, map, coordinates and current direction
     let mut map: Vec<Vec<char>> = Vec::new();
-    let mut sc: (usize, usize) = (0, 0);
+    let mut sc: (i32, i32) = (0, 0);
     let mut direction = Direction::Up;
 
     // Create map
     for (i, line) in file.lines().enumerate() {
         map.push(Vec::new());
 
-        for c in line.chars() {
+        for (j, c) in line.chars().enumerate() {
             map[i].push(c);
             if c == '^' {
-                sc = (i, map[i].len() - 1);
-                let v = map.len() - 1;
-                map[i][v] = 'X';
+                sc = (i as i32, j as i32);
+
+                map[i][j] = 'X';
                 count.0 += 1;
             }
         }
     }
 
-    //Part 1
-    'outer: loop {
-        match map[sc.0][sc.1] {
-            //Go forward
-            '.' => {
-                count.0 += 1;
-                map[sc.0][sc.1] = 'X';
+    // Part 1
+
+    // Clones them for the second part
+    let og_sc = sc.clone();
+    let og_map = map.clone();
+
+    pathy(&mut map, &mut sc, &mut count, &mut direction, true);
+
+    // Part 2
+    for i in 0..map.len() {
+        for j in 0..map[i].len() {
+            // For each 'X' in the first map,
+            // checks whether adding an obstacle creates a loop or not
+            match map[i][j] {
+                'X' => {
+                    // Uses new values every time
+                    let mut map2 = og_map.clone();
+                    let mut sc2 = og_sc.clone();
+                    let mut diron = Direction::Up;
+                    map2[i][j] = 'O';
+                    if pathy(&mut map2, &mut sc2, &mut count, &mut diron, false) {
+                        count.1 += 1;
+                    }
+                }
+                _ => (),
             }
-            //There's an obstacle
-            '#' => {
-                direction = (direction as usize + 1) % 4;
-                //Go back 1
-                dir_update(direction, &mut sc, false);
-            }
-            // Forward, but already seen
-            'X' => {}
         }
-
-        //Next step
-        dir_update(direction, &mut sc, a);
-
-        break 'outer;
     }
 
     count
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 mod days;
 
 fn main() {
-    let path = "src/inputs/i1";
-    let r = days::day1::get_res(path);
-    println!("{:?}", r);
+    /*
+        let path = "src/inputs/i1";
+        let r = days::day1::get_res(path);
+        println!("{:?}", r);
 
-    let path = "src/inputs/i2";
-    let r = days::day2::get_res(path);
-    println!("{:?}", r);
+        let path = "src/inputs/i2";
+        let r = days::day2::get_res(path);
+        println!("{:?}", r);
 
-    let path = "src/inputs/i3";
-    let r = days::day3::get_res(path);
-    println!("{:?}", r);
+        let path = "src/inputs/i3";
+        let r = days::day3::get_res(path);
+        println!("{:?}", r);
 
-    let path = "src/inputs/i4";
-    let r = days::day4::get_res(path);
-    println!("{:?}", r);
+        let path = "src/inputs/i4";
+        let r = days::day4::get_res(path);
+        println!("{:?}", r);
 
-    let path = ("src/inputs/i5.1", "src/inputs/i5.2");
-    let r = days::day5::get_res(path);
-    println!("{:?}", r);
+        let path = ("src/inputs/i5.1", "src/inputs/i5.2");
+        let r = days::day5::get_res(path);
+        println!("{:?}", r);
+    */
 
     let path = "src/inputs/i6";
     let r = days::day6::get_res(path);
