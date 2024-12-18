433be597bec32bcdf95ddf17f6e2bc3b4679bac6
MaxL22 <max987651@gmail.com>
2024-12-18 23:05:43 +0100

    day18 p2

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,14 +1,16 @@
 use std::collections::HashSet;
 
+// Change these to use the test
 const DIM: usize = 71;
 const NUM_STEPS: usize = 1024;
 
+// Keeps the coordinates, distance from the start and heuristic to the end
 #[derive(Clone, Copy, Debug)]
 struct Node {
     x: usize,
     y: usize,
     dist: i32,
-    heur: f32, // Always contains the distance
+    heur: f32, // Also contains the distance
 }
 
 impl Node {
@@ -16,6 +18,7 @@ impl Node {
         Self { x, y, dist, heur }
     }
 
+    // Returns the nodes adjacent to itself
     fn get_adjacent_indices(&self) -> Vec<(usize, usize)> {
         let mut adjacent_indices = Vec::new();
         let directions = [
@@ -38,17 +41,15 @@ impl Node {
     }
 }
 
-pub fn get_res(path: &str) -> (i32, i32) {
-    let file = std::fs::read_to_string(path).unwrap();
-    let mut count = (0, 0);
-
+// Gets the file, parses it and drops the first num_steps bytes
+fn get_path(file: String, num_steps: usize) -> Option<Node> {
     let mut map = [[true; DIM]; DIM];
     let mut q: Vec<(Node, i32)> = Vec::new();
     let mut seen: HashSet<(usize, usize)> = HashSet::new();
 
     // Calculate the map
     for (i, line) in file.lines().enumerate() {
-        if i == NUM_STEPS {
+        if i == num_steps {
             break;
         }
 
@@ -61,6 +62,7 @@ pub fn get_res(path: &str) -> (i32, i32) {
     let mut final_node: Option<Node> = None;
     let mut path: Vec<Node> = Vec::new();
 
+    // It's just A*
     while !q.is_empty() {
         // Get the node
         let val = q.pop().unwrap();
@@ -95,10 +97,50 @@ pub fn get_res(path: &str) -> (i32, i32) {
             ));
         }
 
+        // Sort the q to get the min value, could be a heap
         q.sort_by(|a, b| b.0.heur.total_cmp(&a.0.heur));
     }
 
-    count.0 += final_node.unwrap_or(Node::new(0, 0, 0, 0.0)).dist;
+    // Returns an Option to the final node
+    final_node
+}
+
+pub fn get_res(path: &str) -> (i32, (usize, usize)) {
+    let file = std::fs::read_to_string(path).unwrap();
+    let mut count = (0, (0, 0));
+
+    // Part 1
+    count.0 += get_path(file.clone(), NUM_STEPS)
+        .unwrap_or(Node::new(0, 0, 0, 0.0))
+        .dist;
+
+    // Check the path, one fallen byte at a time
+    let mut row_n: usize = 0;
+    for i in NUM_STEPS..file.clone().lines().count() {
+        let node = get_path(file.clone(), i);
+
+        if let Some(_) = node {
+        } else {
+            // If there's no path, end the loop
+            row_n = i - 1;
+            break;
+        }
+    }
+
+    // Parse the numbers
+    //    (I could've left it as a string)
+    let v: Vec<usize> = file
+        .lines()
+        .nth(row_n)
+        .unwrap()
+        .split(',')
+        .map(|s| s.parse::<usize>().expect("Err"))
+        .collect();
+
+    // But this way it's dumber
+    count.1 .0 = v[0];
+    count.1 .1 = v[1];
 
     count
 }
+// Takes a while, ~18s to end both parts
